3c0e5d6b208331b418b54691d24a1b44e4521f77
uubinn0 <ub.devl0908@gmail.com>
2024-08-03 04:22:08 +0900

    숫자카드 문제 해결

--- SWEA_4834_숫자카드.py
+++ SWEA_4834_숫자카드.py
@@ -1,3 +1,6 @@
+# 평균 시간 복잡도 O(N)
+# 최악의 시간 복잡도 O(N^2) - 해시 테이블이 작동하지 않을꼉우
+
 import sys
 
 sys.stdin = open('4834_input.txt', 'r')
@@ -6,19 +9,33 @@ T = int(input())
 
 for tc in range(1, 1+T):
     N = int(input())
-    num_list = list(map(int, input()))
-    # print(num_list, cnt_list)
+    num_list = list(map(int, input())) #[4, 9, 6, 7, 9]
+    num_set = set(num_list)
 
-    # 딕셔너리를 만들어서
-    # 키 값으로 접근해서 value를 증가시키자
-    num_dict ={}
-    for i in num_list:
-        num_dict[i] = 0
+    max_num = -1
+    max_cnt = 0
 
-    for i in num_list:
-        num_dict[i] += 1
-    
-    print(num_dict)
+    num_dict = dict()
+    for num in num_list:
+        # 숫자 리스트를 돌며 각 숫자의 빈도수를 딕셔너리에 저장
+        # key: 숫자 value: 빈도수
+        if num not in num_dict:
+            # 리스트의 숫자가 딕셔너리에 없는 경우 value를 1로 변경
+            num_dict[num] = 1
+        elif num in num_dict:
+            # 리스트의 숫자가 이미 딕셔너리에 있는 경우 value에 1을 증가시켜줌
+            num_dict[num] += 1
 
+    # 딕셔너리 키값을 순회함
+    for key in num_dict.keys():
+        if max_cnt <= num_dict.get(key) and max_num < key:
+            # 만약 기존 최대 빈도수보다 큰 value를 가지고 있으며
+            # 최대 숫자 보다 key값도 큰 경우에
+            # 각 값을 갱신해줌
+            max_cnt = num_dict.get(key)
+            max_num = key
+
+
+    # print(max(num_dict.values()))
     
-    
+    print(f'#{tc} {max_num} {max_cnt}')
