1a9614b3ad328045975ebd196a74b125ab58b928
uubinn0 <ub.devl0908@gmail.com>
2024-08-16 21:51:56 +0900

    DFS / 시간 기억 안 남

--- 역량테스트 대비/me/SWEA_3234_준환이의양팔저울.py
+++ 역량테스트 대비/me/SWEA_3234_준환이의양팔저울.py
@@ -7,20 +7,19 @@ T = int(input())
 
 def find_way(idx, left, right):
     global result
-    global check
 
     if left < right:    # 왼쪽이 오른쪽보다 작아지는 순간이 생기면 바로 탈출
         return
     
-    if left >= total_weight // 2:   # 현재 왼쪽 무게가 전체 무게의 절반 이상이면
+    if left >= total_weight / 2:   # 현재 왼쪽 무게가 전체 무게의 절반 이상이면
         # 현재 남은 추를 어떤 순서로 어디에 놓아도 모든 경우가 왼쪽이 더 무겁다
         # 따라서 남은 추의 경우의 수만 구하고 더해준다
         result += (2**(N-idx)) * math.factorial(N-idx)
         return
 
-    if idx == N:    # 모든 추를 다 올렸다면 가능한 경우의 수라는 뜻이니까 
-        result += 1 # result 증가 후 종료
-        return
+    # if idx == N:    # 모든 추를 다 올렸다면 가능한 경우의 수라는 뜻이니까 
+    #     result += 1 # result 증가 후 종료
+    #     return
     
     for current_select in range(len(weight)):
         # 현재 고른 추가 올리지 않은 추인 경우에만
@@ -34,7 +33,7 @@ def find_way(idx, left, right):
             # 추 사용 끝났음 체크
             check[current_select] = False
 
-        else: continue  # 이미 사용한 추인 경우 넘어감
+        # else: continue  # 이미 사용한 추인 경우 넘어감
 
 
 for test_case in range(1, T+1):
