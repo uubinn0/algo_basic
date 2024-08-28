dbe95ee8ebc169569b5f63ef70e687cc101a978f
Na Yubin <ub.devl0908@gmail.com>
2024-08-28 13:06:31 +0900

    유니온 부분 문제 해결

--- SWEA/5248_그룹나누기.py
+++ SWEA/5248_그룹나누기.py
@@ -1,3 +1,15 @@
+'''
+대표자가 아닌 노드를 기준으로 다른 그룹과 합칠 때
+해당 노드 이외의 다른 노드 + 대표자의 대표자가 변경되지 않는 문제가 있었음.
+
+ex. 12 34 41인 경우
+마지막에 4와 1을 붙일 때 3의 헤드가 1로 바뀌지 않고 그대로 3으로 유지 됨.
+
+해결하기 위해서 대표자의 대표자 리스트 값을 바꿔주고 난 이후
+자기자신이 대표자가 아닌 값은 해당 대표자의 대표자값으로 한 번 더 변경해줌
+'''
+
+
 T = int(input())
 
 
@@ -21,18 +33,20 @@ for tc in range(1, T+1):
         a_head = find_head(a)
         b_head = find_head(b)
 
+        if a_head == b_head :continue
         # 작은 수를 대표자로 설정할 것임
         # 따라서 큰 수의 대표자 리스트 값을 작은 수로 변경
         if a_head < b_head:
-            head_list[b] = a_head
+            head_list[b_head] = a_head
         else:
-            head_list[a] =  b_head
-    
-    # 부모의 대표자와 자식의 대표자가 같은지 확인 후
-    # 다른 경우 자식의 대표자를 변경해줌
-    for i in range(N+1):
-        if i != head_list[i]:
-            head_list[i] = find_head(head_list[i])
+            head_list[a_head] =  b_head
+        # print(head_list)
+        for j in range(N + 1):
+            if j != head_list[j]:
+                head_list[j] = find_head(head_list[j])
+
+        # print('->',  head_list)
+
 
     # print(head_list)
     group_cnt = len(set(head_list)) -1
