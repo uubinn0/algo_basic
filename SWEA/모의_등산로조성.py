6e167782519d25d22057e7207831a29ae1582f98
uubinn0 <ub.devl0908@gmail.com>
2024-08-30 21:07:08 +0900

    dfs / 2시간 30분

--- SWEA/모의_등산로조성.py
+++ SWEA/모의_등산로조성.py
@@ -1,83 +1,96 @@
-import copy
 from copy import deepcopy
+from pprint import pprint
 
 T = int(input())
 
+
 dyx = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-def search_top(arr):
-    height = 0
 
-    # 가장 높은 봉우리 값 찾기
-    for line in arr:
-        for h in line:
-            height = max(height, h)
+# 최고점 좌표를 찾는 함수
+def top_point(arr):
+    top_num = 0
 
-    top_point = []
-    # 가장 높은 봉우리 좌표 찾기
     for i in range(map_size):
         for j in range(map_size):
-            if arr[i][j] == height:
-                top_point.append((i,j))
+            top_num = max(arr[i][j], top_num)
 
-    return top_point
+    top_list = []
+    for i in range(map_size):
+        for j in range(map_size):
+            if arr[i][j] == top_num:
+                top_list.append((i,j))
+    
+    return top_list
 
 
-def dfs(current_point, length, board):
-    global max_length
-    global chance
+def dfs(current_y, current_x, len, chance, board):
     global visited
+    global max_length
 
-    current_y, current_x = current_point
-    visited[current_y][current_x] = True
+    board = deepcopy(board)
 
     for y, x in dyx:
-        new_y, new_x = current_y + y, current_x + x
+        ny, nx = current_y + y, current_x + x
 
-        # 좌표 밖인 경우 다음 좌표로 넘어간다.
-        if new_y < 0 or new_x < 0 or new_y >= map_size or new_x >= map_size: continue
+        # 해당 좌표가 배열 범위를 벗어났다면 다음 방향의 좌표 생성
+        if ny < 0 or nx <0 or  ny >= map_size or nx >= map_size: continue
 
-        # 이동 가능 범위인 경우 기존 높이보다 높은 곳인 경우
-        if board[new_y][new_x] >= board[current_y][current_x]:
-            # 높이를 깎아도 현재 높이보다 높거나 깎았을 때 높이가 낮더라도 찬스가 없는 경우
-            if board[new_y][new_x] - cut_height >= board[current_y][current_x] or not chance: continue
+        # 방문하지 않은 곳이라면
+        if not visited[ny][nx]:
+            # 현재 위치의 높이보다 다음 위치의 높이가 높은 경우
+            if board[current_y][current_x] <= board[ny][nx]: 
+                # 산을 깎았을 때 높이가 현재 높이보다 높은 경우
+                # 찬스가 있어도 산을 깎는 것이 의미가 없음
+                if board[ny][nx] - cut_num >= board[current_y][current_x]: continue
 
-            # 찬스가 남아 있고 높이를 깎을 수 있다면
-            # 찬스를 쓰고 해당 보드의 좌표 값을 바꿔줘야 한다.
-            # 길의 길이를 늘려주고 다음 좌표에 대해서 다시 이를 반복한다.
+                # 깎으면 갈 수 있지만 찬스가 없음
+                if not chance: continue
 
-            # 방문하지 않은 경우만
-            if not visited[new_y][new_x]:
+                # 여기에 도달한 값은 깎았을 때 이동이 가능한 좌표들
+                # 찬스를 사용해서 높이를 깎고 이동
+                temp = board[ny][nx]
                 chance = False
-                temp_board = copy.deepcopy(board)
-                temp_board[new_y][new_x] = temp_board[current_y][current_x]-1
-                dfs((new_y,new_x), length+1, temp_board)
-                visited[new_y][new_x] = False
+                board[ny][nx] = board[current_y][current_x]-1   # 현재 값보다 하나 작은 값이 더 길게 이동할 수 있음
+                visited[ny][nx] = 1  # 방문 체크
+                dfs(ny, nx, len+1, chance, board)
+                # 바뀐 값들 다시 원상복구
                 chance = True
-
-        # 이동 가능 범위이면서 기존 높이보다 낮은 경우
-        dfs((new_y, new_x), length+1, board)
-
-    max_length = max(max_length, length+1)
+                visited[ny][nx] = 0
+                board[ny][nx] = temp
+                    
+                
+            # 현재 높이보다 낮은 경우 -> 그냥 이동 가능
+            if board[current_y][current_x] > board[ny][nx]:
+                visited[ny][nx] = 1
+                dfs(ny, nx, len+1, chance, board)
+                visited[ny][nx] = 0
+        
+    # pprint(visited)
+    # print('len : ',len)
+    max_length = max(max_length, len)
+    # print('max : ',max_length)
     return
 
 
 for tc in range(1, T+1):
-    map_size, cut_height = map(int, input().split())
+    map_size, cut_num = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(map_size)]
-    visited = [[False]* map_size for _ in range(map_size)]
+    visited = [[0] * map_size for _ in range(map_size)]
     # print(board)
-    # print(visited)
-    chance = True   # 공사 가능 여부
     max_length = 0
 
+    # 가장 높은 지점의 좌표들 찾아서 리스트로 반환
+    top_list = top_point(board)
 
-    # 가장 높은 봉우리 값 찾기
-    top_point = search_top(board)
-    # print(top_point)
+    # 가장 높은 지점들만 dfs돌기
+    for start_y, start_x in top_list:
+        # 시작 지점 방문처리
+        # print('------------------------------------------------------------')
+        visited[start_y][start_x] = 1
+        dfs(current_y =  start_y, current_x = start_x, len=1, chance=True, board=board)
+        # 방문처리 원복
+        # 원복 해줘야 다음 좌표에 대해 DFS를 돌 때 해당 좌표가 방문상태가 아니게 됨
+        visited[start_y][start_x] = 0
 
-    for start_point in top_point:
-        # dfs 멈추는 조건 뭐임?
-        dfs(current_point = start_point, length=0, board = board)
-
-    print(max_length)
+    print(f'#{tc} {max_length}')
