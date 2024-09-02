debdcc2434463aa867482a9b3e3c7fc427da3eca
나유빈 <ub.devl0908@gmail.com>
2024-09-02 17:19:08 +0900

    내부 테케 하나도 안 맞는 쓰레기 코드

--- SWEA/지뢰찾기.py
+++ SWEA/지뢰찾기.py
@@ -4,24 +4,24 @@ from pprint import pprint
 
 T = int(input())
 
-dyx = [[-1,0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1,], [-1, 0], [-1, -1]]
+dyx = [[-1,0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1,], [0, -1], [-1, -1]]
 
 
-def bfs(arr, current_position):
-    global min_click
+def bfs(board, current_position):
+    global arr
     click_cnt = 0
-    arr = deepcopy(arr)
-    pprint(arr)
+    board = deepcopy(board)
+    # pprint(board)
 
     deq = deque([current_position])
 
+    # 각 셀의 인접 폭탄 수로 이중 리스트 초기화
     while deq:
         cy,cx = deq.popleft()
 
         # 방문하거나 연쇄적으로 숫자 표시가 된 적 없는 좌표인 경우
-        if arr[cy][cx] == '.':
-            arr[cy][cx] = 0 # 방문 표시
-            click_cnt += 1  # 클릭 카운트 증가
+        if board[cy][cx] == '.':
+            board[cy][cx] = 0 # 방문 표시
 
             # 8방향 탐색
             for y, x in dyx:
@@ -31,30 +31,62 @@ def bfs(arr, current_position):
                 if ny < 0 or nx < 0 or ny >= N or nx >= N: continue
 
                 # 인접 좌표가 폭탄인 경우 현재 좌표에 인근 폭탄 카운트 증가
-                if arr[ny][nx] == '*':
-                    arr[cy][cx] +=1
+                if board[ny][nx] == '*':
+                    board[cy][cx] +=1
 
                 # 인접 좌표가 폭탄이 아닌 경우 
                 # 탐색할 좌표 후보임
                 # 다음 탐색을 위해 해당 좌표를 큐에 넣음
-                if arr[ny][nx] == '.':
+                if board[ny][nx] == '.':
                     deq.append((ny,nx))
 
-            pprint(arr)
+            # pprint(board)
+    # pprint(board)
 
-    # 다 클릭하고 난 후 안 눌린 . 하나하나 눌러 줌
-    for line in arr:
-        click_cnt += line.count('.')
+    zero_q = []
 
-    min_click = min(min_click, click_cnt)
-    return
+    # 인접 셀 폭탄 수가 0인 셀 클릭 후 주변 셀 열기
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 0:
+                zero_q.append((i, j))
+
+    zero_q = deque(zero_q)
+    # print(zero_q)
+
+    while zero_q:
+        cy, cx = zero_q.popleft()
+
+        # 셀 값이 0이지만 이미 열린 값이라면 넘어감
+        if arr[cy][cx] != '.' and arr[cy][cx] != '*': continue
+
+        # 폭탄이 아니고 숫자인 경우
+        if board[cy][cx] != '*':
+            arr[cy][cx] = board[cy][cx] # 주변 폭탄 수를 원본에 적음
+            click_cnt += 1
+
+            for y, x in dyx:
+                ny, nx = cy + y , cx + x
+
+                # 인접 좌표가 범위를 벗어난 경우 다음 좌표 탐색
+                if ny < 0 or nx < 0 or ny >= N or nx >= N: continue
+
+                # 이미 방문한 곳이라면 pass
+                if arr[ny][nx] != '.': continue
+
+                # 인접 좌표가 폭탄이 아닌 경우 클릭 없이 열림
+                if board[ny][nx] != '*':
+                    arr[ny][nx] = board[ny][nx]
+
+    # pprint(board)
+    # print(click_cnt)
+    return click_cnt
                 
 
 
 for tc in range(1, T+1):
     N = int(input())
     arr = []
-    min_click = float('inf')
 
     for _ in range(N):
         text = input()
@@ -63,12 +95,19 @@ for tc in range(1, T+1):
             temp.append(str)
         arr.append(temp)
 
-    for y in range(N):
-        for x in range(N):
-            # 지뢰가 아닌 좌표에 대해서 bfs 돌기
-            if arr[y][x] != '*':
-                print(y,x)
-                bfs(arr, (y,x))
-                print(min_click)
+    # 폭탄이 아닌 첫 번째 좌표 구하기 함수
+    def find_start(arr, N):
+        for i in range(N):
+            for j in range(N):
+                if arr[i][j] != '*':
+                    return (i,j)
 
-    print(f'#{tc} {min_click}')
+    start_position = find_start(arr,N)
+    # print(start_position)
+    click = bfs(arr, start_position)
+    # pprint(arr)
+
+    for i in range(N):
+        click += arr[i].count('.')
+
+    print(f'#{tc} {click}')
