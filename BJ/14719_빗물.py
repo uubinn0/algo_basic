52904838e573da5e05cb4606f69605195646dca2
uubinn0 <ub.devl0908@gmail.com>
2024-09-24 20:11:12 +0900

    구현 / 너무 어려워서 눈물 흘림

--- BJ/14719_빗물.py
+++ BJ/14719_빗물.py
@@ -4,8 +4,9 @@ https://www.acmicpc.net/problem/14719
 음 하나도 모르겠고
 
 0번과 W-1 인덱스에는 물이 고일 수 없다
-
-1. 배열을 만들어서 벽인 부분은 1로 채우자
+---
+세로로 한 칸씩 보자
+세로 한 줄에 물이 고이기 위해서는 해당 위치의 벽보다 높은 벽이 좌우에 있어야 함
 
 '''
 
@@ -21,25 +22,21 @@ def find_wall(idx):
 
     # 왼쪽 벽 찾기
     for left_idx in range(idx-1, -1, -1):
-        if left_max_height < brick[left_idx]:
-            left_max_height = brick[left_idx]
-            break
+        left_max_height = max(left_max_height, brick[left_idx])
 
     for right_idx in range(idx+1, W):
-        if right_max_height < brick[right_idx]:
-            right_max_height = brick[right_idx]
-            break
+        right_max_height = max(right_max_height, brick[right_idx])
 
     return left_max_height, right_max_height
 
 
 for i in range(1, W-1):
     left_wall, right_wall = find_wall(i)
-    print(left_wall, right_wall)
+    # print(left_wall, right_wall, brick[i])
 
-    # 현재 인덱스에 세워진 벽의 높이가 양 옆 벽의 높이보다 높거나 같은 경우
+    # 어느 한 쪽이라도 자신보다 높은 벽이 없다면 빗물이 고일 수 없으므로 다음으로 넘어감
     if left_wall <= brick[i] or right_wall <= brick[i]: continue
 
     result += min(left_wall, right_wall) - brick[i]
-
+    # print('->', result)
 print(result)
