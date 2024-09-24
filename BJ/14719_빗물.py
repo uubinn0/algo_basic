7b44c7b6483ee1eb53512a6be805209a72b50e32
나유빈 <ub.devl0908@gmail.com>
2024-09-24 17:52:57 +0900

    미완성

--- BJ/14719_빗물.py
+++ BJ/14719_빗물.py
@@ -11,9 +11,35 @@ https://www.acmicpc.net/problem/14719
 
 H, W = map(int, input().split())
 brick = list(map(int, input().split()))
+result = 0
 
-arr = [[0] * W for _ in range(H)]
-# print(arr)
 
-for i in range(W):
-    
+# 좌우의 좌우의 가장 높은 벽 찾기
+def find_wall(idx):
+    left_max_height = 0
+    right_max_height = 0
+
+    # 왼쪽 벽 찾기
+    for left_idx in range(idx-1, -1, -1):
+        if left_max_height < brick[left_idx]:
+            left_max_height = brick[left_idx]
+            break
+
+    for right_idx in range(idx+1, W):
+        if right_max_height < brick[right_idx]:
+            right_max_height = brick[right_idx]
+            break
+
+    return left_max_height, right_max_height
+
+
+for i in range(1, W-1):
+    left_wall, right_wall = find_wall(i)
+    print(left_wall, right_wall)
+
+    # 현재 인덱스에 세워진 벽의 높이가 양 옆 벽의 높이보다 높거나 같은 경우
+    if left_wall <= brick[i] or right_wall <= brick[i]: continue
+
+    result += min(left_wall, right_wall) - brick[i]
+
+print(result)
