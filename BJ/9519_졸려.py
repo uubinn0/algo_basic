410080e585c1dd9714fc7624c2ea17da18523eb5
uubinn0 <ub.devl0908@gmail.com>
2024-10-10 21:27:16 +0900

    왜 돌다가 말아? 틀리면 좋아?

--- BJ/9519_졸려.py
+++ BJ/9519_졸려.py
@@ -13,30 +13,54 @@ https://www.acmicpc.net/problem/9519
 
 N = int(input())
 txt = input()
+cycle = 0
+result = ''
+
+# 단어 길이 2, 3 예외처리
+if len(txt) ==2:
+    print(txt)
+    quit()
+elif len(txt) == 3:
+    if N % 2 == 1:
+        result += txt[0]
+        result += txt[2]
+        result += txt[1]
+        print(result)
+        quit()
+
+
+# 짝수 홀수에 따른 사이클 계산
+if len(txt) % 2 == 0:
+    cycle = len(txt)-1
+else:
+    cycle = len(txt)-2
+
 
 def recover_txt(word):
-    result = ''
+    res = ''
 
     # 짝수일 때
     if len(txt) % 2 ==0:
         # 갈라진 문자들 붙이기
         for i in range(0, len(word)-1, 2):
-            result += word[i]
+            res += word[i]
         # 끼워진 문자들 복구
         for i in range(-1, -len(word), -2):
-            result += word[i]
+            res+= word[i]
 
     else:
         # 갈라진 문자들 붙이기
         for i in range(0, len(word), 2):
-            result += word[i]
+            res += word[i]
         # 끼워진 문자들 복구
         for i in range(-2, -len(word), -2):
-            result += word[i]
+            res= word[i]
 
-    return result
+    return res
 
-for cnt in range(N):
+
+cnt = N % cycle # 단어 복구 횟수
+for _ in range(cnt):
     txt = recover_txt(txt)
 
 print(txt)
